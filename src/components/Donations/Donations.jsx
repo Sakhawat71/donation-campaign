0017185a19b91d8d4b11d8161292748dbb5cf1a2
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-15 04:46:50 +0600

    donations component 4 items show limit

--- src/components/Donations/Donations.jsx
+++ src/components/Donations/Donations.jsx
@@ -6,8 +6,9 @@ import Donation from "../Donation/Donation";
 const Donations = () => {
 
     const donations = useLoaderData();
-    const [donatedCard, setDonatedCard] = useState([])
 
+    const [donatedCard, setDonatedCard] = useState([])
+    const [showCard, setShowCard] = useState(4)
 
     useEffect(() => {
         const donatedId = getStordDonationData();
@@ -21,13 +22,18 @@ const Donations = () => {
 
 
     return (
-        <div className="my-20 grid grid-cols-1 lg:grid-cols-2 gap-6">
-            {
-                donatedCard?.map(donated => <Donation
-                    key={donated.id}
-                    donated={donated}
-                ></Donation>)
-            }
+        <div className="my-20 ">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                {
+                    donatedCard?.slice(0,showCard).map(donated => <Donation
+                        key={donated.id}
+                        donated={donated}
+                    ></Donation>)
+                }
+            </div>
+            <div className="my-10 flex justify-center ">
+                <button onClick={()=>setShowCard(donatedCard.length)}  className={showCard === donatedCard.length ? "btn hidden bg-[#009444] hover:bg-[#009444] text-neutral-50 text-[18px]" :"btn bg-[#009444] hover:bg-[#009444] text-neutral-50 text-[18px]" } >See All</button>
+            </div>
         </div>
     );
 };
