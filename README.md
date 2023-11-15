316e6c7afb3ddd07924cb31bbc28cbfeb48c8704
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-15 17:34:01 +0600

    banner done

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ https://github.com/programming-hero-web-course-4/b8a8-donation-campaign-Sakhawat
 https://donation-campaign-sakhawat71.netlify.app/
 <br/>
 https://donation-campaign-sakhawat71.surge.sh/
+
+
+
+## 3 Project Features 
+
+Welcome to the Donation Campaign React App! This application is designed to facilitate and manage donation campaigns for various causes. Below are three key features of the project:
+
+
+### 
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,18 @@
 
 const Banner = () => {
     return (
-        <div>
-            Banner
+
+        <div className=" mb-20 py-32 relative h-full -mt-28">
+            <div className="bg-cover bg-center absolute top-0 left-0 w-full h-full" style={{ backgroundImage: 'url("/images/bg.png")', opacity: 0.1 }}></div>
+
+            <h1 className="font-bold text-4xl text-center mb-8 text-custom-black relative z-10">I Grow By Helping People In Need</h1>
+
+            <div className="join text-center flex mt-8 justify-center items-center relative z-10">
+                <input className="input input-bordered join-item" placeholder="Search here...." />
+                <button className="btn bg-custom-red text-neutral-50 join-item rounded-r-md" type="button">
+                    Search
+                </button>
+            </div>
         </div>
     );
 };
